00b0552f77086c6a312e3437e0a974ca45f78651
Gerdeth <gerdethwickham@gmail.com>
2020-08-07 17:18:36 +0000

    very basic

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,65 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			current: 0,
+			songs: []
+		};
+		this.player = React.createRef();
+	}
+
+	componentDidMount() {
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+			.then(songs => songs.json())
+			.then(data => this.setState({ songs: data }));
+	}
+
+	playFunction = i => {
+		this.setState({ current: i });
+		this.player.current.src = "files/cartoons/songs/flintstones.mp3";
+		this.player.current.play();
+		// this.player.current.pause();
+	};
+
+	render() {
+		console.log(this.state.songs);
+		return (
+			<div className="text-center mt-5">
+				<ul>
+					<li>
+						<i
+							className="fa fa-step-backward"
+							aria-hidden="true"
+							onClick={this.playFunction}
+						/>
+					</li>
+					<li>
+						<i
+							className="fa fa-play"
+							aria-hidden="true"
+							onClick={this.playFunction}
+						/>
+					</li>
+					<li>
+						<i
+							className="fa fa-pause"
+							aria-hidden="true"
+							onClick={this.playFunction}
+						/>
+					</li>
+					<li>
+						<i
+							className="fa fa-step-forward"
+							aria-hidden="true"
+							onClick={this.playFunction}
+						/>
+					</li>
+				</ul>
+				<audio ref={this.player} />
+			</div>
+		);
+	}
 }
